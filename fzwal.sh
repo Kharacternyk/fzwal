53ca4d82562ef2f1738573f14da54bd9ee3581f9
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-09-01 12:03:14 +0300

    Implement cursor color resetting.

--- fzwal.sh
+++ fzwal.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Lists dark themes by default.
 # Light themes are listed if an argument is passed.
+# Export FZWAL_RESET_CURSOR=1 if you don't want Pywal to change
+# the default behaviour of the terminal cursor (inverse fg and bg).
 
 cp -f ~/.cache/wal/colors.json /tmp/fzwal-backup.json
 
@@ -26,4 +28,11 @@ if [ -n "$THEME" ]; then
     fi
 else
     wal -q --theme /tmp/fzwal-backup.json
+fi
+
+if [ -n "$FZWAL_RESET_CURSOR" ]; then
+    for TTY in /dev/pts/*; do
+        [ -w $TTY ] && printf "\e]112\a" > $TTY
+    done
+    exit 0
 fi
